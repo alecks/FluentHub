7a8bb626fae27ab24b91a9a05f783083eff7d7c0
Alex P <alexeek@protonmail.com>
2020-09-16 20:39:20 +0100

    fix: wrap app in fabric component

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+import { Fabric } from "@fluentui/react";
 import { initializeIcons } from "@uifabric/icons";
 
 initializeIcons();
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Fabric><App /></Fabric>
     </React.StrictMode>,
     document.getElementById("root"),
 );
