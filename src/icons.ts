80cadc3e79fd72ae6f6eca100f81cd1fdcbbbfb2
Alex P <alexeek@protonmail.com>
2020-09-16 20:59:47 +0100

    feat: add Icons enum

---
+++ src/icons.ts
@@ -0,0 +1,12 @@
+const enum Icons {
+    New = "Add",
+    Import = "CloudImportExport",
+    Repository = "ProjectCollection",
+    Gist = "FileCode",
+    Organisation = "BusinessCenterLogo",
+    Project = "BacklogBoard",
+    PullRequest = "BranchPullRequest",
+    Issues = "IssueTracking",
+}
+
+export default Icons;
