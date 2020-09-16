9e7955e881ffab503c940be0572ff16dd65c81d4
Alex P <alexeek@protonmail.com>
2020-09-16 21:36:50 +0100

    feat: add homenav to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import HomeNav from "./components/HomeNav";
 
 export default function App() {
     return (
         <div>
             <Header />
+            <main>
+                <HomeNav />
+            </main>
         </div>
     );
 }
