2f35506e22361aac6de54b4f2754432af1b17aca
Alex P <alexeek@protonmail.com>
2020-09-16 23:17:07 +0100

    fix: use new import locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import HomeNav from "./components/HomeNav";
+import HomeNav from "./components/home";
 
 export default function App() {
     return (
